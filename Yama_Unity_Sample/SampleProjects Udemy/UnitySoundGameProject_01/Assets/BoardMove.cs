9e03f3bbf95bbf6f2ea9c808c134412e3d1bbf4c
fackingbee <fackingbee1983@yahoo.co.jp>
2016-03-07 18:45:45 +0900

    Yama_Unity_Production_20160307

---
+++ Yama_Unity_Sample/SampleProjects Udemy/UnitySoundGameProject_01/Assets/BoardMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardMove : MonoBehaviour {
+
+	// 時間管理用 //
+	// For managing time //
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+
+		// timeを初期化 //
+		// Initialize "time" value //
+		time = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// timeを更新 //
+		// Update the time //
+		time += Time.deltaTime;
+
+		// Boardを時間に応じて自動移動 //
+		// Auto moving for Board at the time. //
+		transform.localPosition = new Vector3( 0, - time * 100, 0 );
+
+	}
+}
